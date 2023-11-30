22801ad65cf98275e0166631b6803db9c8c30ae5
Ajay Shitkar <ajshitkar282@gmail.com>
2023-12-01 00:31:03 +0530

    GitScout

---
+++ README.md
@@ -0,0 +1,6 @@
+# GitScout
+
+- React app to search Github profile using a name or Github username.
+- This app give information about the profile such as recent 5 repositories with their links, number of followers , following and number of public repos and gists is displayed.
+- Implemented this project using React and Github API and managed state using Context API.
+
---
+++ src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className='card all-center'>
+      <h1>About</h1>
+      <p>
+        <i className='fab fa-react'></i> GitScout  a Web app to search{' '}
+        <i className='fab fa-github'></i> Github profiles
+      </p>
+      <p>
+        Made by Ajay Shitkar @2023
+      </p>
+    </div>
+  );
+};
+
+export default About;
